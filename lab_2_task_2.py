27a781d3a04f6d4040605ad666d6a3775d432403
olgichi <anolgi.forgit@gmail.com>
2024-10-27 15:50:51 +0300

    Update lab_2_task_2.py
    Добавила проверку для округления: если денег больше, чем при простом round, то +1

--- lab_2_task_2.py
+++ lab_2_task_2.py
@@ -1,10 +1,15 @@
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
-increase = 0.03  # Ежемесячный рост цен (было дано изначально, ответ совпадает для этого случая)
-# increase = 0.03 # Ежемесячный рост цен (по заданию)
+#increase = 0.03  # Ежемесячный рост цен (было дано изначально в тексте программы)
+increase = 0.05 # Ежемесячный рост цен (по заданию)
 money = 0
 for _ in range(months):
     money = money - spend + salary
     spend = spend * (1 + increase)
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(abs(money)))
+money=abs(money)
+if money>round(money):
+    money=round(money)+1
+else:
+    money=round(money)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money)

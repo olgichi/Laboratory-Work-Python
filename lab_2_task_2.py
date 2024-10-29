a24c0b2002a211b269bdf6e3c725608972351847
olgichi <anolgi.forgit@gmail.com>
2024-10-29 19:56:51 +0300

    Update lab_2_task_2.py
    Исправила

--- lab_2_task_2.py
+++ lab_2_task_2.py
@@ -1,15 +1,12 @@
+import math
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
-#increase = 0.03  # Ежемесячный рост цен (было дано изначально в тексте программы)
-increase = 0.05 # Ежемесячный рост цен (по заданию)
+increase = 0.03  # Ежемесячный рост цен (было дано изначально в тексте программы)
+#increase = 0.05 # Ежемесячный рост цен (по заданию)
 money = 0
 for _ in range(months):
     money = money - spend + salary
     spend = spend * (1 + increase)
-money=abs(money)
-if money>round(money):
-    money=round(money)+1
-else:
-    money=round(money)
+money=math.ceil(abs(money))
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money)

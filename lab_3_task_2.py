60a7fcbe637f0bc3443f82511ca849e10b13afc2
olgichi <anolgi.forgit@gmail.com>
2024-11-05 22:18:28 +0300

    Update lab_3_task_2.py

--- lab_3_task_2.py
+++ lab_3_task_2.py
@@ -10,5 +10,5 @@ participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
-common_participants = find_common_participants(participants_first_group, participants_second_group)
+common_participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
 print(f"Общие участники: {common_participants}")
